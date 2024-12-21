711d8d30fbb5ca9d56e9fda6b3d7ca26a9d803b5
Tim chang <changkaien0101@gmail.com>
2024-12-21 16:18:49 +0800

    feat(layout): implement the home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
-const page = () => {
+const Home = () => {
+  const loggedIn = {
+    firstName: "Timothy",
+    lastName: "Chang",
+    email: "chang@gmail.com",
+  };
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.0 }]}
+      />
+    </section>
+  );
+};
 
-export default page
+export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface HeaderBoxProps {
+  type?: "title" | "greeting";
+  title: string;
+  subtext: string;
+  user?: string;
+}
+
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  );
+};
+
+export default HeaderBox;
